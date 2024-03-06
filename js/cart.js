52e2daa8a5884d703d37d762da2c1992e1511761
ahkamulbd <ahkamulbd@gmail.com>
2024-03-06 17:48:11 +0600

    Case Button Plus & Minus set to increase and decrease respectively after clicking plus and minus btn

---
+++ js/cart.js
@@ -0,0 +1,28 @@
+//console.log('Cart js initiated');
+
+/** Case Button Plus */
+document.getElementById('case-btn-plus').addEventListener('click', function () {
+    //console.log('Case Plus Button Clicked');
+    const caseInputField = document.getElementById('case-input-field');
+    const caseInputString = caseInputField.value;
+    const caseInputNumber = parseInt(caseInputString);
+
+    const previousCaseInputNumber = caseInputNumber;
+    const currentCaseInputNumber = previousCaseInputNumber + 1;
+
+    caseInputField.value = currentCaseInputNumber;
+})
+
+/** Case Button Minus */
+document.getElementById('case-btn-minus').addEventListener('click', function () {
+    //console.log('Case Plus Button Clicked');
+    const caseInputField = document.getElementById('case-input-field');
+    const caseInputString = caseInputField.value;
+    const caseInputNumber = parseInt(caseInputString);
+
+    const previousCaseInputNumber = caseInputNumber;
+    const currentCaseInputNumber = previousCaseInputNumber - 1;
+
+    caseInputField.value = currentCaseInputNumber;
+
+})
